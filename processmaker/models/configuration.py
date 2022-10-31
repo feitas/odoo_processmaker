adeb4fdcfe6c667ce71fd123ebc03125e41bf231
Malijie <tony.ma@cevt.se>
2022-10-31 20:35:19 +0800

    FIX config settings for pm

--- processmaker/models/configuration.py
+++ processmaker/models/configuration.py
@@ -9,14 +9,14 @@ from odoo.exceptions import ValidationError
 class OdooConfiguration(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    pm_url = fields.Char(string='PM Url',required=True)
-    pm_workspace = fields.Char(string='PM Workspace',required=True)
-    pm_client_id = fields.Char(string='PM Client Id',required=True)
-    pm_client_secret = fields.Char(string='PM Client secret',required=True)
-    pm_username = fields.Char(string='PM Username',required=True)
-    pm_password = fields.Char(string='PM Password',required=True)
+    pm_url = fields.Char(string='PM Url',required=True, config_parameter='pm.url')
+    pm_workspace = fields.Char(string='PM Workspace',required=False, config_parameter='pm.workspace')
+    pm_client_id = fields.Char(string='PM Client Id',required=True, config_parameter='pm.client')
+    pm_client_secret = fields.Char(string='PM Client secret',required=True, config_parameter='pm.client.secret')
+    pm_username = fields.Char(string='PM Username',required=True, config_parameter='pm.username')
+    pm_password = fields.Char(string='PM Password',required=True, config_parameter='pm.password')
     
-    pm_user_name = fields.Char(string='Name of the user of PM',required=True)
+    pm_user_name = fields.Char(string='Name of the user of PM',required=False)
     pm_access_token = fields.Char()
     
     
